85a8fa91c36edbc17bcfe10097821b021c8ca129
Chris McCann <testflyjets@gmail.com>
2012-07-07 16:18:28 -0700

    Adds fileName method and link to file

--- app/models/more_info.rb
+++ app/models/more_info.rb
@@ -2,7 +2,7 @@ class MoreInfo < ParseResource::Base
   
   resource_class_name 'moreInfo'
   
-  fields :fileType, :image, :zapCard
+  fields :fileName, :fileType, :image, :zapCard
   
   belongs_to :zap_card
 
