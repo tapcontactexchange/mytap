6b039157520bd3125795a3de8738bf43089e26b9
Chris McCann <testflyjets@gmail.com>
2012-08-30 12:13:39 -0700

    Adds handling for various file types
    * adds new icons and CSS classes for showing same
    * implements file uploads

--- app/helpers/zap_cards_helper.rb
+++ app/helpers/zap_cards_helper.rb
@@ -3,12 +3,20 @@ module ZapCardsHelper
   def file_link(more_info)
     return nil if more_info.nil?
     
-    file_type = "file_link " + more_info.fileType.to_s
+    file_type = "file_link " + file_link_class(more_info)
     
     display_name = more_info.fileTitle
     display_name = more_info.fileName if display_name.blank?
     display_name = "More Info file" if display_name.blank?
     link_to(display_name, more_info.image.url, :class => file_type)
-    
+  end
+
+  def file_link_class(more_info)
+  	file_type = more_info.fileType.to_s
+  	if ['jpg', 'jpeg', 'png', 'gif'].include?(file_type)
+  		'image'
+  	else
+  		file_type
+  	end
   end
 end
